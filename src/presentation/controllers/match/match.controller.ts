49716830ceceb7f7b73cae44297ffce5285ea3d2
Anthony ELUECQUE <anthony76520.ae@gmail.com>
2025-01-23 01:00:15 +0100

    imp: added response helper & messages from json file

--- src/presentation/controllers/match/match.controller.ts
+++ src/presentation/controllers/match/match.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { MatchService } from "../../services";
 import { MatchLimitExceededError } from "../../errors";
+import { ResponseHelper } from "../../helpers";
+import { StatusCodes } from "../../enums";
+import messages from "../../docs/messages.json";
 
 export default class MatchController {
 
@@ -9,9 +12,9 @@ export default class MatchController {
     static getAll = async (req: Request, res: Response) : Promise<void> => {
         try {
             const matches = await this.service.getAll();
-            res.status(200).json(matches);   
+            ResponseHelper.send(res, StatusCodes.OK, messages.match.getAll, matches);
         } catch (error) {
-            res.status(500).json(error);
+            ResponseHelper.send(res, StatusCodes.CREATED, messages.defaults.serverError, error);
         }
     }
 
@@ -20,14 +23,14 @@ export default class MatchController {
             const userId = parseInt(req.query.userId as string);
             if (userId) {
                 const matches = await this.service.getUserMatches(userId);
-                res.status(200).json(matches);
+                ResponseHelper.send(res, StatusCodes.OK, messages.match.userMatches, matches);
             }   
         } catch (error) {
             if (error instanceof MatchLimitExceededError) {
-                res.status(400).json({ message: "Match limit exceeded" });
+                ResponseHelper.send(res, StatusCodes.BAD_REQUEST, messages.match.matchLimitExceeded);
             }
             else {
-                res.status(500).json(error);
+                ResponseHelper.send(res, StatusCodes.INTERNAL_SERVER_ERROR, messages.defaults.serverError, error);
             }
         }
     }
@@ -37,10 +40,10 @@ export default class MatchController {
             const id = parseInt(req.params.id);
             if (id) {
                 await this.service.deleteMatch(id);
-                res.status(200).json({ message: "Match deleted" });
+                ResponseHelper.send(res, StatusCodes.OK, messages.match.matchDeleted);
             }   
         } catch (error) {
-            res.status(500).json(error);
+            ResponseHelper.send(res, StatusCodes.INTERNAL_SERVER_ERROR, messages.defaults.serverError);
         }
     }
 }

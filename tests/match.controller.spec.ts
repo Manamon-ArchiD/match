d93caa876a6183f2a05fe11f00899c599fa64108
Mistourr <alexisyon42@gmail.com>
2025-01-24 12:15:48 +0100

    :white_check_mark: Added 4 passing tests

--- tests/match.controller.spec.ts
+++ tests/match.controller.spec.ts
@@ -3,6 +3,9 @@ import supertest from 'supertest';
 import { Builder } from '../src/builder';
 import TestAgent from 'supertest/lib/agent';
 import { Match } from '../src/models/match.entity';
+import { NewMatchDto } from '../src/presentation/dto/new-match.dto';
+import { UpdateMatchDto } from '../src/presentation/dto/update-match.dto';
+import { MatchStatus } from '../src/presentation/enums';
 
 describe('Database Test', () => {
     let orm : MikroORM;
@@ -33,5 +36,36 @@ describe('Database Test', () => {
         const response = await request.get(`${endpoint}/user?userId=1`);
         expect(response.status).toBe(200);
         expect(response.body).toHaveLength(2);
+    });
+
+    it('should add a match', async () => {
+        const body: NewMatchDto = {
+            invitations: [],
+            public: true,
+            userId: "1"
+        };
+        const response = await request.post(endpoint).send(body);
+        expect(response.status).toBe(201);
+    });
+
+    it('should update a match', async () => {
+        const body: UpdateMatchDto = {
+            status: MatchStatus.ENDED
+        }
+        const response = await request.put(`${endpoint}/1`).send(body);
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("finishedAt")
+    });
+    
+    it('should get a match by id', async () => {
+        const response = await request.get(`${endpoint}/1`);
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("id");
+        expect(response.body.id).toBe(1);
+    });
+
+    it('should delete a match', async () => {
+        const response = await request.delete(`${endpoint}/1`);
+        expect(response.status).toBe(200);
     });
 });
